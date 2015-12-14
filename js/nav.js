b7d1f2d6eb1a1f40f52f4870fb26861100f97425
Kevin_Pellet_(Ilphrin) <ilphrin@autistici.org>
2015-12-14 22:49:35 +0100

    Some UI modification Two new articles And some responsive improvments

--- js/nav.js
+++ js/nav.js
@@ -1,11 +1,12 @@
 function contact_popup(nyah) {
   var plop = document.querySelector('body');
   var child = document.querySelector('.contact_pop');
-  if (getComputedStyle(child).marginLeft === "-350px") {
+  var width = child.children[0].offsetWidth;
+  if (getComputedStyle(child).marginLeft === "-" + width + "px") {
     child.style.marginLeft = "0px";
   }
   else
   {
-    child.style.marginLeft = "-350px";
+    child.style.marginLeft = "-" + width + "px";
   }
 }
