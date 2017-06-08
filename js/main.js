9c0001d3c3506d8bb2fba1b407a0a94a0917a592
Kevin Ilphrin Pellet <ilphrin@autistici.org>
2017-06-08 19:43:00 +0200

    New version of the homepage and design

--- js/main.js
+++ js/main.js
@@ -1,106 +1,129 @@
+"use strict";
 /**
  * Hexagon
- * 
+ *
  * @link  {string} external link for element
  * @bg    {string} background image url
  * @title {string} content title
  * @desc  {string} detail about the content
  */
 
-function Hexagon(link, bg, title, desc) {
-  this.link = link;
-  this.bg = bg;
-  this.title = title;
-  this.desc = desc;
-}
+$(window).on("load", () => {
+  class Hexagon {
+    constructor(link, bg, title, desc) {
+      this.link = link;
+      this.bg = bg && bg.getAttribute('src');
+      this.title = title;
+      this.desc = desc;
+    }
 
-var fillHexagon = function(data, element) {
-  let svg = SVG('svg' + data.title.innerHTML);
-  let pattern = svg.pattern(300, 300, function(add) {
-    add.rect(300,300);
-    add.fill(add.image(data.bg.getAttribute('src'), 300, 300));
-    add.stroke({color: '#000', width: 3});
-  });
-  svg.attr({fill: pattern});
-};
-
-var positionHexagon = function(element, index, length) {
-  let TOP;
-  if (index % 2 == 0) {
-    TOP = 150;
-  }
-  else {
-    TOP = 280;
-  }
-  const PAD = -75;
-
-  element.style.left = "calc(-75px + " + (index * PAD - (length / 2 * PAD)).toString() + "px)";
-  element.style.top = TOP.toString() + "px";
-}
-
-var fillWorkHexagon = function() {
-  let hexagon = new Hexagon();
-  let baseLeft = "50%";
-
-  let works = document.querySelectorAll("#work .hexag");
-  if (works) {
-   let index = 0;
-    for (work of works) {
-      let content = work.children[1];
-
-      hexagon.link = content.children[1];
-      hexagon.title = content.children[0];
-      hexagon.desc = content.children[2];
-      hexagon.bg = content.children[3];
-      fillHexagon(hexagon);
-      positionHexagon(work, index, works.length);
-      index++;
+    toString(element) {
+      if (this[element] instanceof Element) {
+        this[element] = this[element].innerHTML;
+      }
+      else if (this[element] === undefined) {
+        return "";
+      }
+      return this[element];
     }
   }
-};
 
-var fillBlogHexagon = function() {
-  let hexagon = new Hexagon();
-  let post = document.querySelector("#blog .hexag");
-  if (post) {
-    post = post.children[1];
+  var fillHexagon = function(data) {
+    let svg = SVG('svg' + data.toString("title"));
+    const pattern = svg.pattern(300, 300, function(add) {
+      add.rect(300,300);
+      add.fill(add.image(data.toString("bg"), 300, 300));
+      add.stroke({color: '#000', width: 3});
+    });
+    svg.attr({fill: pattern});
+  };
 
-    hexagon.title = post.children[0];
-    hexagon.desc = "";
-    hexagon.link = post.children[1];
-    hexagon.bg = post.children[2];
-    console.log(hexagon.bg);
+  var positionHexagon = function(element, index, length) {
+    let TOP;
+    if (index % 2 == 0) {
+      TOP = 150;
+    }
+    else {
+      TOP = 280;
+    }
+    const PAD = -75;
 
-    fillHexagon(hexagon);
-  }
-}
-
-var fillHeaderCurrentArticle = function() {
-  let hexagon = new Hexagon();
-  let post = document.querySelector("#trending_article .hexag");
-  if (post) {
-    post = post.children[1];
-
-    hexagon.title = post.children[0];
-    hexagon.desc = "";
-    hexagon.link = "";
-    hexagon.bg = post.children[1];
-    fillHexagon(hexagon);
+    if (index > 0) {
+      console.log(index * PAD - (length / 2 * PAD));
+      element.style.left = "calc(-112.5px + " + (index * PAD - (length / 2 * PAD)).toString() + "px)";
+    }
+    element.style.top = TOP.toString() + "px";
   }
 
-  post = document.querySelector("#header_article .hexag");
-  if (post) {
-    post = post.children[1];
+  var fillWorkHexagon = function() {
+    const baseLeft = "50%";
 
-    hexagon.title = post.children[0];
-    hexagon.desc = "";
-    hexagon.link = "";
-    hexagon.bg = post.children[1];
-    fillHexagon(hexagon);
+    const works = document.querySelectorAll("#work .hexag");
+    if (works) {
+     let index = 0;
+      for (work of works) {
+        const content = work.children[1].children;
+        let hexagon = new Hexagon(content[1], content[3], content[0], content[2]);
+        fillHexagon(hexagon);
+        positionHexagon(work, index, works.length);
+        index++;
+      }
+    }
+  };
+
+  var fillBlogHexagon = function() {
+    let hexagon = new Hexagon();
+    let post = document.querySelector("#blog .hexag");
+
+    if (post) {
+      post = post.children[1];
+
+      hexagon.title = post.children[0];
+      hexagon.desc = "";
+      hexagon.link = post.children[1];
+      hexagon.bg = post.children[2];
+
+      fillHexagon(hexagon);
+    }
   }
-}
 
-fillHeaderCurrentArticle();
-fillWorkHexagon();
-fillBlogHexagon();
+  var fillHeaderCurrentArticle = function() {
+    let hexagon = new Hexagon();
+    let post = document.querySelector("#trending_article .hexag");
+    if (post) {
+      post = post.children[1];
+
+      hexagon.title = post.children[0];
+      hexagon.desc = "";
+      hexagon.link = "";
+      hexagon.bg = post.children[1];
+      fillHexagon(hexagon);
+    }
+
+    post = document.querySelector("#header_article .hexag");
+    if (post) {
+      post = post.children[1];
+
+      hexagon.title = post.children[0];
+      hexagon.desc = "";
+      hexagon.link = "";
+      hexagon.bg = post.children[1];
+      fillHexagon(hexagon);
+    }
+  }
+
+  var fillBrand = function() {
+    let hp = document.querySelector("#brand .hexag");
+    if (hp) {
+      let hexagon = new Hexagon("", hp.children[1].children[0], "brand", "");
+      fillHexagon(hexagon);
+    }
+  }
+
+  fillHeaderCurrentArticle();
+  fillWorkHexagon();
+  fillBlogHexagon();
+  fillBrand();
+
+});
 
